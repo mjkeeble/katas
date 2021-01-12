ceb6777b2210840efd8a585e8ddf7810038dbb5c
Mark Keeble <mjkeeble@yahoo.de>
2021-01-12 11:10:30 +0100

    up to date

--- katas.js
+++ katas.js
@@ -288,20 +288,298 @@ function top3(products, amounts, prices) {
     return result.slice(0, 3)
 }
 
-// ======== 
+// ======== Roman Numerals Decoder
+// https://www.codewars.com/kata/51b6249c4612257ac0000005
+
+function solution(roman) {
+    const numerals = ["I", "V", "X", "L", "C", "D", "M"];
+    const values = [1, 5, 10, 50, 100, 500, 1000];
+    let sum = 0;
+    for (let i = 0; i < roman.length; i++) {
+        let ref = numerals.indexOf(roman[i]);
+        if (numerals.indexOf(roman[i + 1]) > ref) {
+            sum -= values[ref];
+        } else {
+            sum += values[ref];
+        };
+    };
+    return sum;
+}
+
+// ======== Two sum
+// https://www.codewars.com/kata/52c31f8e6605bcc646000082
+function twoSum(numbers, target) {
+    for (i = 0; i < numbers.length; i++) {
+        for (j = i + 1; j < numbers.length; j++) {
+            if (numbers[i] + numbers[j] == target) return [i, j];
+        }
+    }
+}
+
+// alternative
+function twoSum(numbers, target) {
+    for (i = 0; i < numbers.length; i++) {
+        for (j = i + 1; j < numbers.length; j++) {
+            if (numbers[i] + numbers[j] == target) {
+                return [i, j];
+            } else {
+                continue;
+            }
+        }
+    }
+}
+
+// ======== Convert string to camel case
+// https://www.codewars.com/kata/517abf86da9663f1d2000003
+
+function toCamelCase(str) {
+    if (str == "") return "";
+    let words = []
+    if (str.includes('_')) {
+        words = str.split("_");
+    } else {
+        words = str.split("-");
+    }
+    console.log(words)
+    let result = words[0];
+    for (i = 1; i < words.length; i++) {
+        result += words[i].charAt(0).toUpperCase() + words[i].slice(1);
+    }
+    return result;
+}
+
+// ======== Backspaces in string
+// https://www.codewars.com/kata/5727bb0fe81185ae62000ae3
+function cleanString(s) {
+    let newS = ''
+    for (i = 0; i < s.length; i++) {
+        if (s.charAt(i) == "#") {
+            newS = newS.substring(0, newS.length - 1);
+        } else {
+            newS += s.charAt(i)
+        }
+    }
+    return newS
+};
+
+// alternative
+function cleanString(s) {
+    let newS = ''
+    for (i = 0; i < s.length; i++) {
+        if (s.charAt(i) == "#") {
+            newS = newS.substring(0, newS.length - 1);
+        } else {
+            newS += s.charAt(i)
+        }
+        console.log(newS)
+    }
+    return newS
+};
+
+// ======== Pete, the baker
+// https://www.codewars.com/kata/525c65e51bf619685c000059
+function cakes(recipe, available) {
+    // TODO: insert code
+    let minimum = undefined;
+    const requiredIngredients = Object.keys(recipe);
+    console.log(requiredIngredients);
+
+    requiredIngredients.forEach(ingredient => {
+        if (!available.hasOwnProperty(ingredient)) {
+            minimum = 0
+            return
+        }
+        let ingredientLimit = Math.floor(available[ingredient] / recipe[ingredient]);
+        console.log(ingredient, ingredientLimit)
+        if (minimum === undefined) minimum = ingredientLimit
+        minimum = Math.min(minimum, ingredientLimit);
+        console.log(minimum)
+    })
+    return minimum;
+}
+
+// ======== Valid Parentheses
+// https://www.codewars.com/kata/52774a314c2333f0a7000688
+function validParentheses(parens) {
+    let parentheses = 0
+
+    for (let el of parens) {
+        el === '(' ? parentheses++ : parentheses--
+        if (parentheses < 0) return false
+    }
+    return parentheses === 0
+}
+
+// ======== Character with longest consecutive repetition
+// https://www.codewars.com/kata/586d6cefbcc21eed7a001155
+function longestRepetition(s) {
+    let longestRepeat = ["", 0]
+    if (s.length == 0) return longestRepeat
+    let counter = 1
+    for (i = 1; i < s.length; i++) {
+        if (s.charAt(i - 1) == s.charAt(i)) {
+            counter++
+        } else {
+            if (counter > longestRepeat[1]) {
+                longestRepeat = [s.charAt(i - 1), counter]
+            }
+            counter = 1
+        }
+    }
+    if (counter > longestRepeat[1]) return [s.charAt(i - 1), counter]
+    return longestRepeat
+}
+
+// ======== More Zeros than Ones
+// https://www.codewars.com/kata/5d41e16d8bad42002208fe1a
+const moreZeros = (s) => {
+    return [...new Set(s.split('').filter(char => {
+        const bin = char.charCodeAt(0).toString(2);
+        return bin.split('0').length - 1 > bin.split('1').length - 1
+    }))]
+}
+
+
+// ======== Count letters in string
+// https://www.codewars.com/kata/5808ff71c7cfa1c6aa00006d
+function letterCount(s) {
+    const result = {}
+    for (const letter of s) {
+        (letter in result) ? result[letter]++ : result[letter] = 1;
+    }
+    return result
+}
+
+
+// ======== ISBN-10 Validation
+// https://www.codewars.com/kata/51fc12de24a9d8cb0e000001
+function validISBN10(isbn) {
+    let sum = 0
+    if (isbn.length !== 10) return false
+    for (i = 1; i <= 9; i++) {
+        sum += (i * isbn.charAt(i - 1))
+    }
+    isbn.charAt(9) === "X" ? sum += 100 : sum += (isbn.charAt(9) * 10)
+    return (sum % 11 == 0)
+}
+
+
+// ======== The Supermarket Queue
+// https://www.codewars.com/kata/57b06f90e298a7b53d000a86
+function queueTime(customers, n) {
+    if (n === 0 || customers.length === 0) return 0;
+    let tills = customers.splice(0, n);
+    for (let i = 0; i < customers.length; i++) {
+        let indexOfTill = tills.indexOf(Math.min(...tills));
+        tills[indexOfTill] += customers[i];
+    }
+    return Math.max(...tills);
+}
+
+// ======== Guess The Gifts!
+// https://www.codewars.com/kata/52ae6b6623b443d9090002c8
+function guessGifts(wishlist, presents) {
+    let gifts = [];
+    for (let present of presents) {
+        for (let item of wishlist) {
+            if (present.size === item.size &&
+                present.clatters === item.clatters &&
+                present.weight === item.weight &&
+                !gifts.includes(item.name)) {
+                gifts.push(item.name)
+            }
+        }
+    }
+    return gifts
+}
+
+// alternative
+function guessGifts(wishlist, presents) {
+    let gifts = [];
+    for (let present of presents) {
+        for (let item of wishlist) {
+            if (present.size === item.size &&
+                present.clatters === item.clatters &&
+                present.weight === item.weight) {
+                gifts.push(item.name)
+            }
+        }
+    }
+    return [...new Set(gifts)]
+}
+
+// ======== Decode the Morse code
+// https://www.codewars.com/kata/54b724efac3d5402db00065e
+decodeMorse = function(morseCode){
+    const letters = morseCode.trim().split(' ');
+    
+  let message="";
+    for (i=0; i<letters.length;i++) {
+      if (letters[i] !== '') {
+        message += MORSE_CODE[letters[i]];
+      } else {
+        message += " ";
+        i++
+      }
+    }
+    return message;
+  }
+
+// ======== Next bigger number with the same digits
+// https://www.codewars.com/kata/55983863da40caa2c900004e
+function nextBigger(n){
+    let s = n.toString().split('');
+    let pivotIndex = -1;
+    for (i= s.length - 1; i > 0; i--) {
+      if (s[i-1] < s[i]){
+        pivotIndex = i - 1;
+        break;
+      }
+    }
+    
+    if (pivotIndex == -1) return pivotIndex;
+    
+    let right = s.splice(pivotIndex);
+    
+    let pivotValue = right.splice(0, 1);
+    
+    let min = null, minIndex = null;
+    for (i = 0; i < right.length; i++){
+      if (right[i] > pivotValue) {
+        if (min == null || right[i] < min){
+          min = right[i];
+          minIndex = i
+        }
+      }
+    }
+    
+    if (minIndex ==null) return -1;
+    
+    right.splice(minIndex,1);
+    right.push(pivotValue[0]);
+    right = right.sort()
+    
+    let ret = s.concat([min]).concat(right).join('');
+    if(ret < n) return -1
+    
+    return ret * 1
+  
+  }
+
 
 // ======== 
 
-// ======== 
 
 // ======== 
 
-// ======== 
 
 // ======== 
 
-// ======== 
 
 // ======== 
+
+
+// ======== 
+
 
 // ======== 
